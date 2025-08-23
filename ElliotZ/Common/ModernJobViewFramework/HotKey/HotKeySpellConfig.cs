338377b737928559f704ad4896e6baac21c60c82
ElliotZ <elliot_zhu@foxmail.com>
2025-08-23 18:16:33 +0800

    clean for subtree

--- ElliotZ/Common/ModernJobViewFramework/HotKey/HotKeySpellConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable FieldCanBeMadeReadOnly.Global
-namespace ElliotZ.Common.ModernJobViewFramework.HotKey;
-
-public struct HotKeySpell(string n, uint s, int t)
-{
-    public string Name = n;
-    public uint Spell = s;
-    public int Target = t;
-}
