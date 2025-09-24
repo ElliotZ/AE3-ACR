fc5a119cfb26d8991be86ec0e41ca9703c993016
ElliotZ <elliot_zhu@foxmail.com>
2025-09-24 22:05:00 +0800

    fix timeline not saving

--- RprSub/Rpr/Triggers/TriggerActionHotkey.cs
+++ RprSub/Rpr/Triggers/TriggerActionHotkey.cs
@@ -1,6 +1,8 @@
 ﻿using AEAssist.CombatRoutine.Trigger;
 using AEAssist.GUI;
 using ElliotZ.Rpr.QtUI;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ConvertConstructorToMemberInitializers
 
 namespace ElliotZ.Rpr.Triggers;
 
