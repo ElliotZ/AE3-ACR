b8b3693de5402fc96fdf2b2956073654a361bcae
ElliotZ <elliot_zhu@foxmail.com>
2025-10-06 01:45:04 +0800

    fix auto acr mode swap when loading timeline

--- RprSub/Rpr/BattleData.cs
+++ RprSub/Rpr/BattleData.cs
@@ -1,4 +1,6 @@
-﻿namespace ElliotZ.Rpr;
+﻿using AEAssist.CombatRoutine.Module;
+
+namespace ElliotZ.Rpr;
 
 public class BattleData {
   public static BattleData Instance = new();
@@ -9,6 +11,24 @@ public class BattleData {
   public int GcdDuration = 2500;
   public int NumBurstPhases = 0;
   public int HoldCommunio = 0;
+  
+  private static bool _timelineLoaded = false;
+  public static bool TimelineLoaded {
+    get => _timelineLoaded;
+    set {
+      if (_timelineLoaded is false && value != TimelineLoaded) {
+        _timelineLoaded = value;
+        RprHelper.HardCoreMode();
+      }
+
+      if (_timelineLoaded is true && value != TimelineLoaded) {
+        _timelineLoaded = value;
+        if (RprSettings.Instance.AutoSetCasual) {
+          RprHelper.CasualMode();
+        }
+      }
+    } 
+  }
   
   public static bool IsChanged;
   
